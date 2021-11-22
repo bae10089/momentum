ce041962d3a7ec4c8cea4601cfd7135bfb43b23c
bae10089 <91697764+bae10089@users.noreply.github.com>
2021-11-22 20:07:26 +0900

    123

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-function sayHello(name, age){
-    console.log("Hello! "+name+" "+ age);
-}
-sayHello("jiho", 30);
+const title = document.querySelector(".title h1");
 
-function plus(a, b){
-    console.log(a+b);
+
+console.dir(title);
+function handleTitleClick() {
+    title.style.color ="yellow";    
 }
-plus(5,7);
+function handleMouseEnter(){
+    title.innerText="mouse is here!"
+}
+function handleMouseLeave(){
+    title.innerText ="mouse is gone!"
+}
+function handleWindowResize() {
+    document.body.style.backgroundColor ="tomato" ;
+}
+function handleWindowCopy (){
+    alert("copier!");
+}
+title.onclick = handleTitleClick;
+title.onmouseenter = handleMouseEnter;
+title.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+
