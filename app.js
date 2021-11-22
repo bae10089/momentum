8b6b26fd3d81c1638723ef99bc960c5b572e08c7
bae10089 <91697764+bae10089@users.noreply.github.com>
2021-11-23 00:23:19 +0900

    123

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 const title = document.querySelector(".title h1");
 
-
 console.dir(title);
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
 function handleTitleClick() {
-    title.style.color ="yellow";    
+  if(title.style.color ==="blue"){
+      title.style.color = "tomato"
+  }
+  else{
+      title.style.color = "blue"
+  }
 }
-function handleMouseEnter(){
-    title.innerText="mouse is here!"
-}
-function handleMouseLeave(){
-    title.innerText ="mouse is gone!"
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
 }
 function handleWindowResize() {
-    document.body.style.backgroundColor ="tomato" ;
+  document.body.style.backgroundColor = "tomato";
 }
-function handleWindowCopy (){
-    alert("copier!");
+function handleWindowCopy() {
+  alert("copier!");
+}
+function handleWindowOffLine() {
+  alert("SOS no WIFI");
+}
+function handleWinodwOnline() {
+  alert("good");
 }
 title.onclick = handleTitleClick;
+
 title.onmouseenter = handleMouseEnter;
 title.onmouseleave = handleMouseLeave;
-
 window.addEventListener("resize", handleWindowResize);
 window.addEventListener("copy", handleWindowCopy);
-
+window.addEventListener("offline", handleWindowOffLine);
+window.addEventListener("online", handleWinodwOnline);
+{
+  (" ");
+}
